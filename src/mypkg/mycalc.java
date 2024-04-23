efa7d698d855f6e48a25f62c15e1caf4531a0c0b
Hima Bindu Golla <himabindug2424@gmail.com>
2024-04-23 23:58:47 +0530

    first commit sum added

---
+++ src/mypkg/mycalc.java
@@ -0,0 +1,16 @@
+package mypkg;
+
+public class mycalc {
+	int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		mycalc calc=new mycalc();
+		System.out.println("Thse sum is"+calc.sum(10,20));
+		// TODO Auto-generated method stub
+
+	}
+
+}
